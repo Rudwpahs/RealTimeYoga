d798792ed8fa994d2655dc4d1dfe5e9b59584511
Rudwpahs <110889799+Rudwpahs@users.noreply.github.com>
2022-08-27 17:55:43 +0900

    Update test_angle.py

--- test_angle.py
+++ test_angle.py
@@ -4,9 +4,27 @@ import time
 import math
 import numpy as np
 
+def Trackbar(img):
+    def onChange(x):
+        pass
+
+    img = cv2.imread("TREE-M.png")
+
+    cv2.namedWindow("Pose Estimation")
+    cv2.createTrackbar('Man or Woman', "Pose Estimation", 0, 1, onChange)
+    while True:
+
+        WM = cv2.getTrackbarPos('Man or Woman', "Pose Estimation")
+        if WM == 1:
+            img = cv2.imread("TREE-W.png")
+        else:
+            img = cv2.imread("TREE-M.png")
+        cv2.imshow("Pose Estimation", img)
+
+
 class poseDetector():
-    def __init__(self, mode= False, complexity= 1, landmarks= True, enable_seg= False,
-                 smooth_seg= True, det_conf= 0.5, track_conf= 0.5):
+    def __init__(self, mode=False, complexity=1, landmarks=True, enable_seg=False,
+                 smooth_seg=True, det_conf=0.5, track_conf=0.5):
         self.mode = mode
         self.complexity = complexity
         self.landmarks = landmarks
@@ -20,6 +38,7 @@ class poseDetector():
         self.pose = self.mpPose.Pose(self.mode, self.complexity, self.landmarks,
                                      self.enable_seg, self.smooth_seg, self.det_conf,
                                      self.track_conf)
+
     def findPose(self, img, draw=True):
 
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -27,18 +46,18 @@ class poseDetector():
         if draw:
             if self.results.pose_landmarks:
                 self.mpDraw.draw_landmarks(img, self.results.pose_landmarks,
-                                       self.mpPose.POSE_CONNECTIONS)
+                                           self.mpPose.POSE_CONNECTIONS)
 
         return img
 
-    def findPosition(self, img, draw= True):
+    def findPosition(self, img, draw=True):
         lmlist = []
         if self.results.pose_landmarks:
             for id, lm in enumerate(self.results.pose_landmarks.landmark):
                 h, w, c = img.shape
-                #print(id, lm)
+                # print(id, lm)
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                lmlist.append([id,cx,cy])
+                lmlist.append([id, cx, cy])
                 if draw:
                     cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
         return lmlist
@@ -46,36 +65,38 @@ class poseDetector():
 
 def main():
     cap = cv2.VideoCapture(0)
-    cap.set(3,1280)
-    cap.set(4,640)
+    cap.set(3, 480)
+    cap.set(4, 640)
     pTime = 0
     detector = poseDetector()
     image = cv2.imread("TREE-M.png")
+    Trackbar(image)
     while True:
         success, img = cap.read()
         img = detector.findPose(img)
         lmlist = detector.findPosition(img)
-        #print(lmlist)
+        # print(lmlist)
         if len(lmlist) > 0:
             print(lmlist[11])
             print(lmlist[12])
             print(lmlist[13])
             angle = calculateAngle(lmlist[12], lmlist[11], lmlist[13])
-            #print(angle)
+            # print(angle)
             cv2.circle(img, (lmlist[11][1], lmlist[11][2]), 10, (0, 0, 255), 2)
             cv2.circle(img, (lmlist[12][1], lmlist[12][2]), 10, (0, 0, 255), 2)
             cv2.circle(img, (lmlist[13][1], lmlist[13][2]), 10, (0, 0, 255), 2)
 
-            cv2.putText(img, str(int(angle)), (70,100), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 1)
+            cv2.putText(img, str(int(angle)), (70, 100), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 1)
 
         cTime = time.time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
         cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (205, 30, 140), 3)
 
-        cv2.imshow("Pose Estimation", img)
-        #cv2.imshow("Pose Estimation", np.hstack((img, image)))
+        # cv2.imshow("Pose Estimation", img)
+        cv2.imshow("Pose Estimation", np.hstack((img, image)))
         cv2.waitKey(1)
+
 
 def calculateAngle(landmark1, landmark2, landmark3):
     _, x1, y1 = landmark1
@@ -90,7 +111,5 @@ def calculateAngle(landmark1, landmark2, landmark3):
     return angle
 
 
-
 if __name__ == "__main__":
     main()
-   
